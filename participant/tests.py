0895afe6acdd9f955faa9966748641479dcd0fa4
Alex Pereira Vieira <alexpvieira@users.noreply.github.com>
2022-03-11 11:26:42 -0300

    Update tests.py

--- participant/tests.py
+++ participant/tests.py
@@ -5,19 +5,24 @@ from participant.models import Participant
 class ParticipantTestCase(TestCase):
     def test_participant_creation(self):
         """Teste da criação de um novo participante {name, age}"""
+        pass
 
     
     def test_participant_list_all(self):
         """Teste da listagem de todos os participantes"""
+        pass
 
 
     def test_participant_list_one(self):
         """Teste da listagem de um participante"""
+        pass
 
 
     def test_participant_update(self):
         """Teste da atualização de um participante"""
+        pass
 
 
     def test_participant_delete(self):
         """Teste da exclusão de um participante"""
+        pass
